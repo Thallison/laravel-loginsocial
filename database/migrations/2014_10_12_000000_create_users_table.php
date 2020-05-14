25d14f5299320407a3f23879f4dbdabc075830a7
Morais Thallison <t.morais@reply.com>
2020-05-14 11:08:33 -0300

    Adicionando tabela user os campos para autenticação facebook/github

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('email_github')->nulltable();
+            $table->string('email_facebook')->nulltable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
